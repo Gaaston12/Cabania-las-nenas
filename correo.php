99aa59bfdd0a6dd1dcaeeda65f0494ed6894a0fd
Gaaston12 <Gaastonmaartin12@gmail.com>
2020-07-16 16:40:19 -0300

    galeria terminada

--- correo.php
+++ correo.php
@@ -1,5 +1,5 @@
 <?php
-    $destinatario = 'gaastonmaartin12@gmail.com';
+    $destinatario = 'Gaastonmaartin12@gmail.com';
 
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
@@ -9,7 +9,7 @@
     $header = "Enviado desde la pagina de Cabaña las Nenas";
     $mensajeCompleto = $mensaje . "\nAtentamente:" . $nombre;
 
-    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    mail($destinatario, $asunto, $mensajeCompleto, $email);
     echo "<script> alert('correo enviado con exito');</script>";
-    echo "<script> setTimeout(\"location.href='index.html'\",1000);</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\",500);</script>";
 ?>
--- js/app.js
+++ js/app.js
@@ -13,3 +13,29 @@ jQuery('document').ready(function ($) {
     });
 
 });
+
+//galeri
+const images = document.querySelectorAll('.img');
+const containerImage = document.querySelector('.container-img');
+const imageContainer = document.querySelector('.img-show');
+const copy = document.querySelector('.copy');
+const closeModal = document.querySelector('.icon-cross');
+
+images.forEach(image =>{
+    image.addEventListener('click', ()=>{
+        addImage(image.getAttribute('src'), image.getAttribute('alt'));
+    })
+})
+
+const addImage = (srcImage, altImage)=>{
+    containerImage.classList.toggle('move');
+    imageContainer.classList.toggle('show');
+    imageContainer.src = srcImage;
+    copy.innerHTML = altImage;
+}
+
+closeModal.addEventListener('click',()=>{
+    containerImage.classList.toggle('move');
+    imageContainer.classList.toggle('show');
+    
+})
